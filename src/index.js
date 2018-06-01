3ec13d6ef85a40a230c7bbf8804d7a181ae9ce7e
Tarang Hirani <tarang9211@gmail.com>
2018-06-01 14:54:28 +0530

    fix: added ping route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const corsProxy = require("cors-anywhere");
 const app = express();
 
 const host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment constiable
 const port = process.env.PORT || 4231;
 
-const corsProxy = require("cors-anywhere");
+app.get("/ping", (req, res, next) => {
+  res.status(200).json({ message: "pong" });
+});
+
 corsProxy
   .createServer({
     originWhitelist: [], // Allow all origins

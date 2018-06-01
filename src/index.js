14c65a544b2541cd877816b9e3f427524041065d
Tarang Hirani <tarang9211@gmail.com>
2018-06-01 14:37:07 +0530

    setup express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+
 const host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment constiable
 const port = process.env.PORT || 4231;
